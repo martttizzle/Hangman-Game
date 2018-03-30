88a5098b733d00e4b2726f837003d345d9e10122
Martel Ramdin <martel1983@yahoo.com>
2018-03-29 20:06:46 -0500

    add all updates from laptop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,16 @@
 
 var astronomers = ["GalileoGalilei","Hipparchus","EdwinHubble","StevenHawking"];
 var underScoreArry =[];//storing the amount of underscores;
+var userInputs = []; //the user inputs from keyboard.
 var stringSize;
 
 //Randomizing the number for array
 var randNum = Math.floor(Math.random()*astronomers.length);
 var astroPeople = astronomers[randNum];
+
 // Storing number value of length of astronomers name
 stringSize = astroPeople.length;
+
 // Generating underscores and putting in to array. 
 function underScoreGenerator (amt) {
     for(var i = 0; i <= amt; i++){
@@ -15,20 +18,25 @@ function underScoreGenerator (amt) {
     }
     return underScoreArry;
 }
-    
+   
+//Make the function call and input length of name for function args
 var displayUnderScore = underScoreGenerator(stringSize).join("   ");
 
+//USER Input
 
-
-console.log(underScoreArry.join(" "));
-
-
-
-
+document.addEventListener('keydown', function(event) {
+    console.log("------> "+ event.keyCode);
+    var res = String.fromCharCode(event.keyCode);
+    document.getElementById("test3").innerHTML = res;
+    
+});
+ 
 
 
 
 //testing onto HTML 
-document.getElementById("test").innerHTML = astroPeople;
+//document.getElementById("test").innerHTML = astroPeople;
 document.getElementById("test2").innerHTML = displayUnderScore;
+//document.getElementById("test3").innerHTML = guesses;
+
 
