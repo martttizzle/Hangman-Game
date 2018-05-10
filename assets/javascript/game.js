556666d0f51dae6c4062dbfa9b566cd8ab6b9286
Vinod <ramdinv@gmail.com>
2018-05-10 17:41:17 -0500

    changed the if statemetns

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 
-var planets = ["MARS"];//, "EARTH", "JUPITER", "NEPTUNE", "URANUS", "PLUTO", "SATURN", "MERCURY", "VENUS"
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var planets = ["MARSA"];//, "EARTH", "JUPITER", "NEPTUNE", "URANUS", "PLUTO", "SATURN", "MERCURY", "VENUS"
+var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var underScoreArry = [];//storing the amount of underscores;
 console.log(underScoreArry);
 var wrongInpt = [""]; //the user inputs from keyboard.
@@ -14,16 +14,17 @@ var plntSplt;
 var stringSize;
 
 
+document.getElementById("guessR").innerHTML = guessesRemaining = 5;
 randomPlanet()
-
 
 //Randomizing the number for array
 function randomPlanet() {
+
     rmdPlnt = planets[Math.floor(Math.random() * planets.length)];
     plntSplt = rmdPlnt.split("");
     stringSize = rmdPlnt.length;
-    document.getElementById("guessR").innerHTML = guessesRemaining = 5;
-    underScoreGenerator(stringSize).join("   ");
+
+    underScoreGenerator(stringSize).join(" ");
 }
 //Generating and displaying underscores
 function underScoreGenerator(amt) {
@@ -40,39 +41,52 @@ document.onkeyup = function (event) {
     var guess = String.fromCharCode(event.keyCode);
     checker(guess);
 }
+debugger;
 //Function to check if guess is correct or not.
+
 function checker(guess) {
     if (plntSplt.indexOf(guess) > -1) {
         for (var i = 0; i < plntSplt.length; i++) {
             if (plntSplt[i] === guess) {
                 underScoreArry[i] = guess;
-                // var s = plntSplt.splice(i, 1, " ");
+                var removed = plntSplt.splice(i, 1, " ");
+                wrongInpt.push(guess);
                 document.getElementById("entered").innerHTML = underScoreArry.join(' ');
                 break;
             }
         }
-    } else if (alphabet.indexOf(guess) > -2) {
-        for(var i = 0; i < alphabet.length; i++) {
-            var s = alphabet.splice(i, 1, " ");
-            break;
-        alert(s);
-     }
-        wrongInpt.push(guess);
-        document.getElementById("wrongL").innerHTML = wrongInpt.join(' ');
-        document.getElementById("guessR").innerHTML = guessesRemaining--;
-       
-    }else{
-        alert('nothing');
+    } else if (plntSplt.indexOf(guess) === -1) {
+        for (var u = 0; u < wrongInpt.length; u++) {
+            if (wrongInpt[u] === guess) {
+                alert('That letter ' + guess + ' was already used')
+                break;
+            }
+            else if(wrongInpt !== guess) {
+                removeAlpabet(guess);
+            }
+        }
     }
+}//end of checker function
 
+function removeAlpabet(guess) {
+    for (var x = 0; x < alphabet.length; x++) {
+        if (alphabet[x] === guess && wrongInpt.indexOf(guess) === -1) {
+            var s = alphabet.splice(x, 1, " ");
+            wrongInpt.push(s);
+            document.getElementById("wrongL").innerHTML = wrongInpt.join(' ');
+
+            guessesRemaining--;
+            document.getElementById("guessR").innerHTML = guessesRemaining;
+            break;
+        }
+    else if (alphabet[x] !== guess ) {
+        alert("Not in the alphabet list")
+    }
+    else{
+        alert("already guessed");
+    }
+    }
 }
-
-
-
-
-
-
-
 //document.getElementById("test").innerHTML = rmdPlnt;
 //document.getElementById("test3").innerHTML = guesses;else{document.querySelector(".img").style.backgroundImage = planetImg[3];
 
